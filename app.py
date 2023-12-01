f698f7bf4c2080f92eb1f8c7766d80ed1292f51d
Carl Molina <carlandrew.molina@gmail.com>
2023-11-30 16:44:30 -0800

    Before Code Review 1

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
 
+responses = []
+
 @app.get('/')
 def get_survey():
     """Routes user to survey_start w/ title and instructions variables"""
@@ -32,21 +34,55 @@ def handle_form_submit():
 
 
 
-@app.get('/questions/0')
-def handle_questions():
+@app.get('/questions/<int:num>')
+def handle_questions(num):
     """Handles questions from redirect"""
-    question_0 = survey.questions[0]
+
+    print('This is num', num)
+    question = survey.questions[num]
+
+    # Pseudocode for reference:
     # replace the h1 with the q0's prompt
     # pass in question instance
     # 2 radio buttons -- Jinja for loop,
         # question_0.choices[0] - first choice
         # question_0.choices[1] - 2nd choice
         #["yes", "no"]
-    # 
-    return render_template("question.html", question=question_0)
+    #
+    return render_template("question.html", question=question, num=num)
 
 
+@app.post('/answer/<int:num>')
+def get_answer(num):
+    """Gets answer from submitted form; appends answer to response list;
+    redirects to either the next question or to thank you.
+    """
+
+    answer = request.form["answer"]
+
+    responses.append(answer)
+    print('This is responses', responses)
+
+    num += 1
+
+    # Pseudocode for reference:
+    # check if anymore questions, if no more questions, return a redirect to
+    # thank you page
+
+    if (num < len(survey.questions)):
+        return redirect(f"/questions/{num}")
+    else:
+        return redirect("/thanks")
 
 
-# TODO: add user's responses from survey to go here
-responses = []
+@app.get("/thanks")
+def thanks():
+    """Redirects user to thank you page after all questions answered"""
+
+    prompts = [question.prompt for question in survey.questions]
+
+    q_a = zip(prompts, responses)
+    print('This is q_a', q_a)
+
+
+    return render_template("completion.html", responses=q_a)
